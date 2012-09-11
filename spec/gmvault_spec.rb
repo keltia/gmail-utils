4c4a455e17269fa6b7386e0cf04968493025ac9a
Ollivier Robert <roberto@keltia.net>
2012-09-11 12:07:14 +0200

    Add GMail specs.

---
+++ spec/gmvault_spec.rb
@@ -0,0 +1,17 @@
+# @abstract GMail-specific specs
+#
+# @author Ollivier Robert <roberto@keltia.net>
+#
+# $Id: gmvault_spec.rb,v 0e232010aa72 2012/09/11 10:07:14 roberto $
+
+require "rspec"
+require "gmail/gmvault"
+
+describe GMail do
+
+  it "should do something" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
