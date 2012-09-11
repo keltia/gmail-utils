0a4f848ba5beb432dd4e77c55a4be6115002f72b
Ollivier Robert <roberto@keltia.net>
2012-09-11 21:45:13 +0200

    Specs for class GMail.

--- spec/gmvault_spec.rb
+++ spec/gmvault_spec.rb
@@ -2,16 +2,86 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 #
-# $Id: gmvault_spec.rb,v 0e232010aa72 2012/09/11 10:07:14 roberto $
+# $Id: gmvault_spec.rb,v a2ae461f4ec7 2012/09/11 19:45:13 roberto $
 
 require "rspec"
 require "gmail/gmvault"
 
 describe GMail do
 
-  it "should do something" do
+  before(:all) do
+    @tag = "Perso/Foo"
+    @goodmail = GMail.new(File.expand_path(File.dirname(__FILE__) + '/../test/good.meta'))
+    @badmail = GMail.new(File.expand_path(File.dirname(__FILE__) + '/../test/bad.meta'))
+  end
 
-    #To change this template use File | Settings | File Templates.
-    true.should == false
+  describe "#initialize" do
+    it "should raise an exception if no parameter is given" do
+      expect{GMail.new}.to RaiseError(ArgumentError)
+    end
+
+    it "should raise an exception if filename does not end with .eml" do
+      expect{GMail.new("foo.bar")}.to RaiseError(ArgumentError)
+    end
+
+    it "should raise an exception if file does not exist" do
+      expect{GMail.new("/nonexistent")}.to RaiseError(ArgumentError)
+    end
+
+    it "should have the necessary attributes" do
+      @goodmail.name.should be_an_instance_of(String)
+      @goodmail.meta.should be_an_instance_of(NilClass)
+      @goodmail.mail.should be_an_instance_of(Mail)
+      @goodmail.tags.should be_an_instance_of(Array)
+      @goodmail.tags.should == []
+    end
+  end
+
+  describe "#load" do
+    it "should return the mail if tag is nil" do
+      @goodmail.load(nil).should_not be_nil
+      @badmail.load(nil).should_not be_nil
+    end
+
+    it "should return the mail if tag is nil" do
+      @goodmail.load(nil).should_not be_nil
+      @badmail.load(nil).should_not be_nil
+    end
+
+    it "should return a gm_id for a matching mail" do
+      @goodmail.load(tag).should_not be_nil
+    end
+
+    it "should return nil for a non-matching mail" do
+      @badmail.load(tag).should be_nil
+    end
+  end
+
+  describe "#save" do
+    it "should save the mail in the given maildir mailbox"
+  end
+
+  describe "#match" do
+    it "should return false if mail does not have the tag" do
+      @badmail.match(@tag).should be_false
+    end
+
+    it "should return true if mail has the tag" do
+      @goodmail.match(@tag).should be_true
+    end
+  end
+
+  describe "#meta_path" do
+    it "should give you back a .meta filename" do
+      @badmail.meta_path.should eq("bad.meta")
+      @goodmail.meta_path.should eq("good.meta")
+    end
+  end
+
+  describe "#mail_path" do
+    it "should give you back a .eml filename" do
+      @badmail.meta_path.should eq("bad.eml")
+      @goodmail.meta_path.should eq("good.meta")
+    end
   end
 end
