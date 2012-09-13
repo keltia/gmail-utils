ff5fce8ca0e907feb9754bd244fecdf11c1a94ae
Ollivier Robert <roberto@keltia.net>
2012-09-13 17:46:20 +0200

    Handle to the TC db.

--- lib/gmail/gmindex.rb
+++ lib/gmail/gmindex.rb
@@ -2,14 +2,14 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 
-VCS_GMI_ID = "$Id: gmindex.rb,v f3a4da3ed2d3 2012/09/12 14:13:00 roberto $"
+VCS_GMI_ID = "$Id: gmindex.rb,v a31bbe7943fe 2012/09/13 15:46:20 roberto $"
 
 require "rufus/tokyo"
 
 # Manages a database of message-id from mail converted from gmvault into Maildir
 #
 class GmIndex
-  attr_reader :path
+  attr_reader :path, :db
 
   def initialize(path)
     if File.exists?("#{path}/cur") and File.exists?("#{path}/new")
