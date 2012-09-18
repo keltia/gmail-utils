330e424cb4870261cba8e17724c079bff88f7654
Ollivier Robert <roberto@keltia.net>
2012-09-18 11:08:21 +0200

    Add specs for #present? & #save.

--- spec/gmindex_spec.rb
+++ spec/gmindex_spec.rb
@@ -2,17 +2,20 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 #
-# $Id: gmindex_spec.rb,v fceaf6eb2d38 2012/09/13 16:07:40 roberto $
+# $Id: gmindex_spec.rb,v 4cbb5180fbdc 2012/09/18 09:08:21 roberto $
 
 require "rspec"
 require "mail"
+require "maildir"
 require "gmail/gmvault"
 require "gmail/gmindex"
 
 describe GmIndex do
 
   before(:all) do
+    @md = Maildir.new(File.expand_path(File.dirname(__FILE__) + '/../test/Perso-Foo'))
     @ind = GmIndex.new(File.expand_path(File.dirname(__FILE__) + '/../test/Perso-Foo'))
+    @mail = GMail.new(File.expand_path(File.dirname(__FILE__) + '/../test/1412679471642059988.meta'))
   end
 
   describe "#initialize" do
@@ -26,11 +29,19 @@ describe GmIndex do
   end
 
   describe "#save" do
-    it "should save the given message-id in the db"
+    it "should save the given message-id in the db" do
+      @ind[@mail.mail["gm_id"]] = true
+      @ind[@mail.mail["gm_id"]].should_not be_nil
+      @ind.db[@mail.mail["gm_id"]].should_not be_nil
+    end
   end
 
   describe "#present?" do
-    it "should check whether a given message-id is present"
+    it "should return false if not present" do
+      @ind.present?("foo-bar-baz").should be_false
+      @ind[@mail.mail["gm_id"]] = true
+      @ind.present?(@mail.mail["gm_id"]).should be_true
+    end
   end
   
 end
