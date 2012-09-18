cd79651d65bef221f31c95e645db5f493938d7f8
Ollivier Robert <roberto@keltia.net>
2012-09-18 11:08:46 +0200

    Fix code according to failing specs.

--- lib/gmail/gmindex.rb
+++ lib/gmail/gmindex.rb
@@ -2,7 +2,7 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 
-VCS_GMI_ID = "$Id: gmindex.rb,v 9cb95816af03 2012/09/18 09:07:18 roberto $"
+VCS_GMI_ID = "$Id: gmindex.rb,v c7d70eb97799 2012/09/18 09:08:46 roberto $"
 
 require "rufus/tokyo"
 
@@ -20,10 +20,26 @@ class GmIndex
     @db = Rufus::Tokyo::Cabinet.new("#{path}/index.tch")
   end
 
+  # Check if a given gm_id is present
   # @param gm_id GMail ID of the message
   # @return [TrueClass|FalseClass] true if present
   def present?(gm_id)
-    false if @db[gm_id].nil?
+    return false if @db[gm_id].nil?
     true
   end
+
+  # Allow getting a value
+  # @param gm_id GMail ID of the message
+  # @return [TrueClass|FalseClass] whether it has been stored
+  def [](gm_id)
+    @db[gm_id]
+  end
+
+  # Allow inserting a new value
+  # @param gm_id GMail ID of the message
+  #
+  def []=(gm_id, value)
+    @db[gm_id] = value
+  end
+
 end
