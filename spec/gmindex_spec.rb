5e69109c0fcfebca749652c1a560bd6c141ba2ea
Ollivier Robert <roberto@keltia.net>
2012-09-18 17:50:12 +0200

    More specs for #save & #present?.

--- spec/gmindex_spec.rb
+++ spec/gmindex_spec.rb
@@ -2,7 +2,7 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 #
-# $Id: gmindex_spec.rb,v 4cbb5180fbdc 2012/09/18 09:08:21 roberto $
+# $Id: gmindex_spec.rb,v c76853b5a5b0 2012/09/18 15:50:12 roberto $
 
 require "rspec"
 require "mail"
@@ -30,17 +30,25 @@ describe GmIndex do
 
   describe "#save" do
     it "should save the given message-id in the db" do
-      @ind[@mail.mail["gm_id"]] = true
-      @ind[@mail.mail["gm_id"]].should_not be_nil
-      @ind.db[@mail.mail["gm_id"]].should_not be_nil
+      @mail.gm_id.should_not be_nil
+      @ind[@mail.gm_id] = true
+      @ind[@mail.gm_id].should_not be_nil
+      @ind.db[@mail.gm_id].should_not be_nil
     end
   end
 
   describe "#present?" do
+    it "should return nothing with nil" do
+      @ind.present?(nil).should be_false
+    end
+
     it "should return false if not present" do
       @ind.present?("foo-bar-baz").should be_false
-      @ind[@mail.mail["gm_id"]] = true
-      @ind.present?(@mail.mail["gm_id"]).should be_true
+    end
+
+    it "should return true if present" do
+      @ind[@mail.gm_id] = true
+      @ind.present?(@mail.gm_id).should be_true
     end
   end
   
