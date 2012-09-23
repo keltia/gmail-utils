b3c325088d8719b8f78a4be8baf5efd6777f2980
Ollivier Robert <roberto@keltia.net>
2012-09-23 22:25:55 +0200

    Improve documentation.

--- lib/gmail/tag.rb
+++ lib/gmail/tag.rb
@@ -1,33 +1,39 @@
-# @abstract Handles tag management
+# @abstract Handles GMail tag management
 #
 # @author Ollivier Robert <roberto@keltia.net>
 
-VCS_TAG_ID = "$Id: tag.rb,v 56b33329c4e5 2012/09/17 23:58:18 roberto $"
+VCS_TAG_ID = "$Id: tag.rb,v 312efb7675ea 2012/09/23 20:25:55 roberto $"
 
 # Handles GMail tags
 #
+# Used when creating a mailbox based on the tag itself, after replacing the +"/"+ used
+# by GMail by +"-"+
+#
 class Tag
-  # Handles a tag
-  #
-  # Used when creating a mailbox based on the tag itself, after replacing the "/" used
-  # by IMAP by "-"
-  #
   # label tag's name
-  attr_accessor :label
+  attr_reader :label
 
-  # @param [String] label name of the tag
-  # @return [Tag] the newly created tag
+  # Create a new tag from the parameter
+  #   Tag.new("the/tag") # => a new tag
   def initialize(label)
     @label = label
   end
 
   # Generate a FS-compatible label
+  # If +@label+ is "Foo/Bar"
+  #   self.normalize # => "Foo-Bar"
+  #
   # @return [String] the converted label
   def normalize
     return @label.gsub(%r{/}, '-')
   end
   
-  # Match the tag against another one, string/array
+  # Match the tag against another one or multiple values
+  # If +@label+ is +"Bar"+ then
+  #   self.match("Bar") # => true
+  #   self.match(["Foo", "Bar"]) # => true
+  #   self.match(["tag1", "tag2"]) # => false
+  #
   # @param tags string or array representing a tag or list thereof
   # @return [True|False] rematching result
   def match(tags)
