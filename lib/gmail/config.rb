e9763256d9817e9bc8a7591f98b64c46723d25aa
Ollivier Robert <roberto@keltia.net>
2012-10-23 01:11:01 +0200

    Specs, code and test data for GMail::Config.

--- lib/gmail/config.rb
+++ lib/gmail/config.rb
@@ -4,7 +4,7 @@
 #
 # XXX Assume we use OAuth for all accounts
 
-VCS_CFG_ID = "$Id: config.rb,v d710b2d69339 2012/10/22 15:14:52 roberto $"
+VCS_CFG_ID = "$Id: config.rb,v 341f2ce15d43 2012/10/22 23:11:01 roberto $"
 
 # Non-standard packages
 #
@@ -17,6 +17,28 @@ BASE_DIR = ENV["HOME"] + "/.gmvault"
 #
 module GMail
   class Config
+    attr_reader :path
 
+    def initialize(path = "#{ENV["HOME"] + "/.gmvault"}")
+      @path = path
+    end
+
+    def find_addrs
+      @list = Array.new
+      Dir.chdir(@path) do
+        Dir["**.oauth"].each do |m|
+          @list << m.split(".")[0..-2].join(".")
+        end
+      end
+      @list
+    end
+
+    def list
+      @list || find_addrs()
+    end
+
+    def self.list(path)
+      self.new(path).list
+    end
   end # -- Config
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,20 +2,45 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 #
-# $Id: config_spec.rb,v d710b2d69339 2012/10/22 15:14:52 roberto $
+# $Id: config_spec.rb,v 341f2ce15d43 2012/10/22 23:11:01 roberto $
 
 require "rspec"
 require "gmail"
 
+FAKE_ADDR="keltia+fake@gmail.com"
+TEST_DIR="test/dot.gmvault"
+
 describe GMail::Config do
   attr_reader :list
 
+  before(:all) do
+    @cfg = GMail::Config.new(TEST_DIR)
+  end
+
   describe "#initialize" do
-    it "should create a Config object"
+    it "should not be nil" do
+      @cfg.should_not be_nil
+    end
+
+    it "should create a Config object" do
+      @cfg.should be_an_instance_of(GMail::Config)
+    end
+
+    it "should set the right members" do
+      @cfg.path.should eq(TEST_DIR)
+    end
+  end
+
+  describe "#list" do
+    it "should get all the configured addresses" do
+      @cfg.list.should eq([FAKE_ADDR])
+    end
   end
 
   describe ".list" do
-    it "should get the default address"
+    it "should get all the configured addresses" do
+      GMail::Config.list(TEST_DIR).should eq([FAKE_ADDR])
+    end
   end
 
   describe ".recent" do
