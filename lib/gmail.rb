e81d3e6691fb6bd3a0c65f1972568bea981fe0fd
Ollivier Robert <roberto@keltia.net>
2012-10-22 16:34:16 +0200

    Add loading of GMail::Config.

--- lib/gmail.rb
+++ lib/gmail.rb
@@ -2,8 +2,9 @@
 #
 # @author Ollivier Robert <roberto@keltia.net>
 #
-# $Id: gmail.rb,v 004b15a184a6 2012/10/18 21:45:32 roberto $
+# $Id: gmail.rb,v e6575f415994 2012/10/22 14:34:16 roberto $
 
+require "gmail/config"
 require "gmail/core"
 require "gmail/tag"
 require "gmail/entity"
