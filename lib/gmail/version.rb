dc08a0a9188ea28e59a124671630f4843d0ec8f7
Ollivier Robert <roberto@keltia.net>
2012-12-04 00:49:58 +0100

    Wrapping up everything in a bug-fix 0.5.1.

--- lib/gmail/version.rb
+++ lib/gmail/version.rb
@@ -3,7 +3,7 @@ module GMail
   # Just to handle these constants
   module Utils
     # Main API version
-    VERSION = "0.5.0"
+    VERSION = "0.5.1"
     # Database version
     DB_VERSION = "3"
   end
