9f3691c81337462a1ba50f65c5ad79a57c6f3c31
Ollivier Robert <roberto@keltia.net>
2013-05-24 21:10:26 +0200

    Welcome 0.5.2.

--- lib/gmail/version.rb
+++ lib/gmail/version.rb
@@ -3,7 +3,7 @@ module GMail
   # Just to handle these constants
   module Utils
     # Main API version
-    VERSION = '0.5.1'
+    VERSION = '0.5.2'
     # Database version
     DB_VERSION = '3'
   end
